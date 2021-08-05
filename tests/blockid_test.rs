f0d8368c8c2ebede1c1867b8a96e0997df9eae0f
yuziroppe <yahata.dev@gmail.com>
2021-08-05 13:42:41 +0900

    blockid test

---
+++ tests/blockid_test.rs
@@ -0,0 +1,16 @@
+use simple_db::db::blockid::BlockId;
+
+#[test]
+fn test_display() {
+    let filename = String::from("test");
+    let blknum = 10;
+
+    let bi = BlockId::new(filename.clone(), blknum);
+
+    assert_eq!(filename, bi.filename());
+    assert_eq!(blknum, bi.number());
+    assert_eq!(
+        format!("[file {}, block {}]", filename, blknum),
+        format!("{}", bi)
+    )
+}
