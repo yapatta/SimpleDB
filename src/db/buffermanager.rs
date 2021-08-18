997ff367f5c382627cb8860659c90451c04aeb33
yuziroppe <yahata.dev@gmail.com>
2021-08-19 01:43:18 +0900

    on the way

--- src/db/buffermanager.rs
+++ src/db/buffermanager.rs
@@ -6,7 +6,7 @@ use super::logmanager::LogMgr;
 use std::cell::RefCell;
 use std::fmt;
 use std::rc::Rc;
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 
@@ -20,15 +20,16 @@ impl fmt::Display for BufferMgrError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             BufferMgrError::LockFailed(s) => {
-                write!(f, "lock failed: {}", s)
+                write!(f, "lock failed function: {}", s)
             }
         }
     }
 }
 
 pub struct BufferMgr {
-    bufferpool: Arc<RwLock<Vec<Buffer>>>,
-    num_available: Arc<Mutex<usize>>,
+    bufferpool: Vec<Buffer>,
+    num_available: usize,
+    l: Arc<Mutex<bool>>,
 }
 
 impl BufferMgr {
@@ -43,44 +44,49 @@ impl BufferMgr {
             .collect();
 
         BufferMgr {
-            bufferpool: Arc::new(RwLock::new(bufferpool)),
-            num_available: Arc::new(Mutex::new(numbuffs)),
+            bufferpool,
+            num_available: numbuffs,
+            l: Arc::new(Mutex::new(true)),
         }
     }
 
     pub fn flush_all(&mut self, txnum: i32) -> Result<()> {
-        if let Ok(mut bufferpool) = self.bufferpool.write() {
-            for i in 0..bufferpool.len() {
-                if bufferpool[i].modifying_tx() == txnum {
-                    bufferpool[i].flush()?;
+        if let Ok(_) = self.l.lock() {
+            for i in 0..self.bufferpool.len() {
+                if self.bufferpool[i].modifying_tx() == txnum {
+                    self.bufferpool[i].flush()?;
                 }
             }
         }
 
-        Ok(())
+        Err(From::from(BufferMgrError::LockFailed(
+            "flush_all".to_string(),
+        )))
     }
 
     pub fn unpin(&mut self, buff: &mut Buffer) -> Result<()> {
-        buff.unpin();
+        if let Ok(_) = self.l.lock() {
+            buff.unpin();
 
-        if !buff.is_pinned() {
-            if let Ok(mut num_available) = self.num_available.lock() {
-                *num_available += 1;
-            } else {
-                return Err(From::from(BufferMgrError::LockFailed(String::from(
-                    "num_available",
-                ))));
+            if !buff.is_pinned() {
+                self.num_available += 1;
             }
+
+            return Ok(());
         }
 
-        Ok(())
+        Err(From::from(BufferMgrError::LockFailed(String::from(
+            "unpin",
+        ))))
     }
 
     // TODO: 燃え尽きた、あとで
-    pub fn pin(&mut self, blk: &BlockId) {
-        //if self.try_to_pin(blk).is_some() {
-        //
-        //      }
+    pub fn pin(&mut self, blk: &BlockId) -> Result<()> {
+        if let Ok(_) = self.l.lock() {}
+
+        while let Some(buff_index) = self.try_to_pin(blk) {}
+
+        Ok(())
     }
 
     pub fn try_to_pin(&mut self, blk: &BlockId) -> Option<usize> {
@@ -91,32 +97,26 @@ impl BufferMgr {
             if let Some(i) = self.choose_unpinned_buffer() {
                 buff_index = i as i32;
 
-                if let Ok(mut bufferpool) = self.bufferpool.write() {
-                    if bufferpool[i].assign_to_block(blk.clone()).is_err() {
-                        return None;
-                    }
+                if self.bufferpool[i].assign_to_block(blk.clone()).is_err() {
+                    return None;
                 }
             }
         }
 
         if buff_index >= 0 {
-            if let Ok(mut bufferpool) = self.bufferpool.write() {
-                bufferpool[buff_index as usize].pin();
+            self.bufferpool[buff_index as usize].pin();
 
-                return Some(buff_index as usize);
-            }
+            return Some(buff_index as usize);
         }
 
         None
     }
 
     pub fn find_existing_buffer(&mut self, blk: &BlockId) -> Option<usize> {
-        if let Ok(bufferpool) = self.bufferpool.read() {
-            for i in 0..bufferpool.len() {
-                if let Some(b) = bufferpool[i].block() {
-                    if *b == *blk {
-                        return Some(i);
-                    }
+        for i in 0..self.bufferpool.len() {
+            if let Some(b) = self.bufferpool[i].block() {
+                if *b == *blk {
+                    return Some(i);
                 }
             }
         }
@@ -125,11 +125,9 @@ impl BufferMgr {
     }
 
     pub fn choose_unpinned_buffer(&mut self) -> Option<usize> {
-        if let Ok(bufferpool) = self.bufferpool.read() {
-            for i in 0..bufferpool.len() {
-                if !bufferpool[i].is_pinned() {
-                    return Some(i);
-                }
+        for i in 0..self.bufferpool.len() {
+            if !self.bufferpool[i].is_pinned() {
+                return Some(i);
             }
         }
 
